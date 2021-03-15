8a9a332b601d54a0e27ffc28a423674d576b60d5
xpalaa00 <78907070+xpalaa00@users.noreply.github.com>
2021-03-15 10:04:51 +0100

    Přidejte soubory projektu.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cv05_bpc_oop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
